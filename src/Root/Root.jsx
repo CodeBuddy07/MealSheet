80f3980366f82cc1222d653e0c999d34181bdbe9
Ruhul <brainboostservices@gmail.com>
2024-07-07 19:23:26 +0600

    Sidebar has been updated with balance div and working navLinks

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 
 
 const Root = () => {
@@ -20,13 +20,42 @@ const Root = () => {
             <div className="drawer-side">
                 <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
 
-                <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
-                    <div className=" text-right">
+                <ul className="menu bg-base-200 text-base-content min-h-full min-w-80 p-4">
+                    <div className=" text-right pr-2 pb-2">
                         <label htmlFor="my-drawer" className="drawer-button"><i className="fa-solid fa-lg fa-xmark"></i></label>
                     </div>
+
+                    <div className="w-full p-5 bg-[url('./balanceDivBG.jpg')] bg-cover rounded-md">
+
+                        <div className="bg-white w-full pr-2 py-2 rounded-md flex justify-start items-center gap-2">
+                            <img className="w-10" src="./dollarSign.png" alt="" />
+                            <div>
+                                <p className=" mb-2 font-semibold ">Balance</p>
+                                <p className="font-black text-xl text-green-500">৳ <span className="">120.00</span></p>
+                            </div>
+                        </div>
+
+
+                        <div className="flex gap-2 mt-2 justify-center items-center">
+                            <div className="bg-white w-full px-2 py-1 rounded-md">
+                                <p className="text-xs font-bold mb-2 ">Deposit</p>
+                                <p className="font-black  text-green-500">৳ <span className="">320.00</span></p>
+                            </div>
+                            <div className="bg-white w-full px-2 py-1 rounded-md">
+                                <p className="text-xs font-bold mb-2 ">Cost</p>
+                                <p className="font-black  text-green-500">৳ <span className="">320.00</span></p>
+                            </div>
+                        </div>
+
+
+                    </div>
+                    
                     {/* Sidebar content here */}
-                    <li><a>Sidebar Item 1</a></li>
-                    <li><a>Sidebar Item 2</a></li>
+                    <li><NavLink className={({ isActive }) => isActive ? "bg-green-200 border-l-2 border-l-green-400 font-semibold focus:bg-green-200 active:!bg-green-400" : "focus:bg-green-200 active:!bg-green-400"} to={"/"}>Home</NavLink> </li>
+                    <li><NavLink className={({ isActive }) => isActive ? "bg-green-200 border-l-2 border-l-green-400 font-semibold focus:bg-green-200 active:!bg-green-400" : "focus:bg-green-200 active:!bg-green-400"} to={"/bazar"}>Bazar</NavLink> </li>
+                    <li><NavLink className={({ isActive }) => isActive ? "bg-green-200 border-l-2 border-l-green-400 font-semibold focus:bg-green-200 active:!bg-green-400" : "focus:bg-green-200 active:!bg-green-400"} to={"/dashboard"}>Dashboard</NavLink> </li>
+
+
                 </ul>
             </div>
         </div>
