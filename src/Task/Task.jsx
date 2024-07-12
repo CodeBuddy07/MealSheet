df161262b695e30146bbeba5c2e410db5bebe588
Ruhul <brainboostservices@gmail.com>
2024-07-12 15:28:30 +0600

    try to fixing date input

--- src/Task/Task.jsx
+++ src/Task/Task.jsx
@@ -36,10 +36,10 @@ const Task = () => {
         return date.getDate();
     }
 
-    function manageAdd(e){
+    function manageAdd(e) {
         e.preventDefault();
         const TaskName = e.target.taskName.value;
-        data.map(user =>  e.target[`${user.user_name}assignedDate`].value? user.tasks? user.tasks.push({"name" : TaskName, "assigned_date": e.target[`${user.user_name}assignedDate`].value}): user.tasks = [{"name" : TaskName, "assigned_date": e.target[`${user.user_name}assignedDate`].value}] : true);
+        data.map(user => e.target[`${user.user_name}assignedDate`].value ? user.tasks ? user.tasks.push({ "name": TaskName, "assigned_date": e.target[`${user.user_name}assignedDate`].value }) : user.tasks = [{ "name": TaskName, "assigned_date": e.target[`${user.user_name}assignedDate`].value }] : true);
         taskData.push(TaskName);
         e.target.reset();
         setVisible(false);
@@ -50,86 +50,86 @@ const Task = () => {
 
         const listItems = document.querySelectorAll(".try");
         listItems.forEach(element => {
-            if(element.innerText.toLowerCase().search(searchTerm2.toLowerCase())> -1){
+            if (element.innerText.toLowerCase().search(searchTerm2.toLowerCase()) > -1) {
                 element.scrollIntoView({ behavior: "smooth", block: "center", inline: "center" });
             }
         });
-        
+
     }
 
-//user.tasks.push({"name" : TaskName, "assigned_date": e.target[`${user.user_name}assignedDate`].value})
+    //user.tasks.push({"name" : TaskName, "assigned_date": e.target[`${user.user_name}assignedDate`].value})
     return (
-        <div className="p-5">
-            <div>
-                <button onClick={() => setVisible(true)} className="bg-slate-200 rounded-md w-full py-3 font-bold">ADD <i className="fa-solid fa-xl fa-circle-plus"></i></button>
-                {taskData.map((task, index) =>
-                    <div key={index} className="grid place-items-center p-3 bg-slate-200 mt-5 rounded-md">
-                        <h1 className="text-lg font-bold">{task}</h1>
-                        <div className="w-full flex items-center mt-3">
-                            <input onChange={(e) =>  setSearchTerm(e.target.value + "," + task) } className="border w-full bg-white rounded-full px-3 py-2 shadow-sm" type="search" name="searchBar" placeholder="search here..." />
-                            <button className="bg-white rounded-full px-3 py-2 ml-2 shadow-sm"><i className="fa-solid fa-magnifying-glass"></i></button>
-                        </div>
+        <div className="p-5 ">
 
-                        <div className="w-full mt-5">
-                            <ul className="w-full *:mt-2">
-                                {
-                                    data.map(user => user.tasks?.filter(x => x.name == task).filter(x => searchTerm.includes(task) ? getWeekDay(x.assigned_date).toLowerCase().search(searchTerm.replace(`,${task}`, "").toLowerCase()) > -1 || getMonthDay(x.assigned_date).toString().search(searchTerm.replace(`,${task}`, "")) > -1 || user.user_name.toLowerCase().search(searchTerm.replace(`,${task}`, "").toLowerCase()) > -1 : x.name == task).map((y, index) =>
-                                        <li key={index} className="flex justify-between items-center w-full bg-blue-200 rounded-md px-3 py-2" >
-                                            <div className="text-center text-white">
-                                                <h1 className="text-3xl font-bold">{getMonthDay(y.assigned_date)}</h1>
-                                                <p className="font-semibold text-sm -pb-5">{getWeekDay(y.assigned_date)}</p>
-                                            </div>
-                                            <div className="flex justify-start items-center gap-3">
-                                                <h1 className=" font-bold text-lg">{user.user_name}</h1>
-                                                <img className="w-12 rounded-full border-2 border-blue-500" src="/Cat03.jpg" alt="" />
-                                            </div>
-                                        </li>
-                                    ))
-
-
-                                }
-
-
-                            </ul>
-                        </div>
+            <button onClick={() => setVisible(true)} className="bg-slate-200 rounded-md w-full py-3 font-bold">ADD <i className="fa-solid fa-xl fa-circle-plus"></i></button>
+            {taskData.map((task, index) =>
+                <div key={index} className="grid place-items-center p-3 bg-slate-200 mt-5 rounded-md">
+                    <h1 className="text-lg font-bold">{task}</h1>
+                    <div className="w-full flex items-center mt-3">
+                        <input onChange={(e) => setSearchTerm(e.target.value + "," + task)} className="border w-full bg-white rounded-full px-3 py-2 shadow-sm" type="search" name="searchBar" placeholder="search here..." />
+                        <button className="bg-white rounded-full px-3 py-2 ml-2 shadow-sm"><i className="fa-solid fa-magnifying-glass"></i></button>
                     </div>
-                )}
-                <div className={visible ? "bg-black/30 backdrop-blur-sm absolute bottom-0 top-0 right-0 left-0 grid place-items-center px-5 " : "hidden"}>
 
-                    <form onSubmit={(e)=>manageAdd(e)} className="bg-white rounded-md p-5 mt-5 mb-60 w-full">
-
-                        <h1 className="font-bold">Task Name</h1>
-                        <input required className="px-5 py-2 bg-slate-200 rounded-full w-full" placeholder="Type here..." name="taskName" type="text" />
-
-                        <h1 className="font-bold mt-3">Assigned To</h1>
-                        <div className="w-full flex items-center mt-5">
-                            <input onChange={(e)=> setSearchTerm2(e.target.value)} className="border w-full bg-slate-200 rounded-full px-3 py-2 shadow-sm" type="search" name="searchBar" placeholder="search here..." />
-                            <button onClick={(e)=>manageSearch(e)} className="bg-slate-200 rounded-full px-3 py-2 ml-2 shadow-sm"><i className="fa-solid fa-magnifying-glass"></i></button>
-                        </div>
-                        <ul className=" mt-2 *:mt-2 text-ellipsis whitespace-nowrap overflow-scroll h-44">
+                    <div className="w-full mt-5">
+                        <ul className="w-full *:mt-2">
                             {
-                                data.map((user,index) =>
-                                    <li key={index} className=" bg-blue-100 flex justify-between items-center px-3 py-2 rounded-md">
-                                        <div className="flex justify-start items-center gap-3">
-                                            <img className="w-8 rounded-full border-2 border-blue-500" src="/Cat03.jpg" alt="" />
-                                            <h1 className=" try font-bold text-sm">{user.user_name}</h1>
+                                data.map(user => user.tasks?.filter(x => x.name == task).filter(x => searchTerm.includes(task) ? getWeekDay(x.assigned_date).toLowerCase().search(searchTerm.replace(`,${task}`, "").toLowerCase()) > -1 || getMonthDay(x.assigned_date).toString().search(searchTerm.replace(`,${task}`, "")) > -1 || user.user_name.toLowerCase().search(searchTerm.replace(`,${task}`, "").toLowerCase()) > -1 : x.name == task).map((y, index) =>
+                                    <li key={index} className="flex justify-between items-center w-full bg-blue-200 rounded-md px-3 py-2" >
+                                        <div className="text-center text-white">
+                                            <h1 className="text-3xl font-bold">{getMonthDay(y.assigned_date)}</h1>
+                                            <p className="font-semibold text-sm -pb-5">{getWeekDay(y.assigned_date)}</p>
                                         </div>
+                                        <div className="flex justify-start items-center gap-3">
+                                            <h1 className=" font-bold text-lg">{user.user_name}</h1>
+                                            <img className="w-12 rounded-full border-2 border-blue-500" src="/Cat03.jpg" alt="" />
+                                        </div>
+                                    </li>
+                                ))
 
-                                        <input  className=" bg-white rounded-md input-xs w-28"  name={`${user.user_name}assignedDate`} type="date" />
-                                    </li>)
+
                             }
-                            
 
 
                         </ul>
-                        <div className=" mt-5 flex justify-end gap-2">
-                            <button onClick={() => setVisible(false)} className="btn btn-sm bg-red-400 text-white">Cancel</button>
-
-                            <input className="btn btn-sm bg-green-400 text-white" type="submit" value="Add" />
-                        </div>
-                    </form>
+                    </div>
                 </div>
+            )}
+            <div className={visible ? "bg-black/30 backdrop-blur-sm absolute top-0 right-0 left-0 bottom-0 grid place-items-center px-5 " : "hidden"}>
+
+                <form onSubmit={(e) => manageAdd(e)} className="bg-white rounded-md p-5 mt-5 mb-60 w-full">
+
+                    <h1 className="font-bold">Task Name</h1>
+                    <input required className="px-5 py-2 bg-slate-200 rounded-full w-full" placeholder="Type here..." name="taskName" type="text" />
+
+                    <h1 className="font-bold mt-3">Assigned To</h1>
+                    <div className="w-full flex items-center mt-5">
+                        <input onChange={(e) => setSearchTerm2(e.target.value)} className="border w-full bg-slate-200 rounded-full px-3 py-2 shadow-sm" type="search" name="searchBar" placeholder="search here..." />
+                        <button onClick={(e) => manageSearch(e)} className="bg-slate-200 rounded-full px-3 py-2 ml-2 shadow-sm"><i className="fa-solid fa-magnifying-glass"></i></button>
+                    </div>
+                    <ul className=" mt-2 *:mt-2 text-ellipsis whitespace-nowrap overflow-scroll h-44">
+                        {
+                            data.map((user, index) =>
+                                <li key={index} className=" bg-blue-100 flex justify-between items-center px-3 py-2 rounded-md">
+                                    <div className="flex justify-start items-center gap-3">
+                                        <img className="w-8 rounded-full border-2 border-blue-500" src="/Cat03.jpg" alt="" />
+                                        <h1 className=" try font-bold text-sm">{user.user_name}</h1>
+                                    </div>
+
+                                    <input className=" bg-white rounded-md input-xs w-28 text-black" name={`${user.user_name}assignedDate`} defaultValue="Select Date" type="date" />
+                                </li>)
+                        }
+
+
+
+                    </ul>
+                    <div className=" mt-5 flex justify-end gap-2">
+                        <button onClick={() => setVisible(false)} className="btn btn-sm bg-red-400 text-white">Cancel</button>
+
+                        <input className="btn btn-sm bg-green-400 text-white" type="submit" value="Add" />
+                    </div>
+                </form>
             </div>
+
         </div>
     );
 };
